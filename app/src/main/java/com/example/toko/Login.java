a1f943b952538f554c439e0e8b6ff1879394acbb
Lenovo <agusahmad20@gmail.com>
2020-07-04 04:08:31 +0700

    update tampilan login

--- app/src/main/java/com/example/toko/Login.java
+++ app/src/main/java/com/example/toko/Login.java
@@ -1,14 +1,27 @@
 package com.example.toko;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Login extends AppCompatActivity {
-
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_login);
+
+        login = findViewById(R.id.b_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+
     }
 }
