6093d17490cad651bc7e7c392367874a56d15b5c
Lenovo <agusahmad20@gmail.com>
2020-07-04 03:06:01 +0700

    Update Splash dan tambahan Login

--- app/src/main/java/com/example/toko/Splash.java
+++ app/src/main/java/com/example/toko/Splash.java
@@ -2,26 +2,16 @@ package com.example.toko;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class Splash extends AppCompatActivity {
-
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a_splash);
-
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent home=new Intent(Splash.this, MainActivity.class);
-                startActivity(home);
-                finish();
-
-            }
-        },1000);
+        startActivity(new Intent(this,Login.class));
+        finish();
     }
 }
